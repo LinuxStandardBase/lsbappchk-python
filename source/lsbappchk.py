54483376eb1eccc950962a19ef3a7c1e5dea51e9
Stew Benedict <stewb@linux-foundation.org>
2008-06-04 15:57:33 -0400

    Finish multiversion support (bug 2098)

--- source/lsbappchk.py
+++ source/lsbappchk.py
@@ -2,7 +2,7 @@
 # prototype application to check python applications
 # for LSB compliance
 # derived from ideas at http://www.tarind.com/depgraph.html
-# Artistic license, Stew Benedict <stewb@linux-foundation.org>
+# Artistic license, Stew Benedict <stewb@linuxfoundation.org>
 
 import sys
 import os
@@ -36,9 +36,11 @@ def file_info(journal, tfile):
 
 # compare the found modules to the LSB list and report
 # if the import uses a /opt path, just warn
-def check_modules(journal, modules, lsb_modules, syspath_org):
+def check_modules(journal, modules, lsb_modules, syspath_org, lsb_version):
     twarn = ' is used from nonstandard path '
     tfail = ' is used, but is not part of LSB'
+    tappeared = ' did not appear until LSB '
+    twithdrawn = ' was withdrawn in LSB '
     keys = modules.keys()
     keys.sort()
     tcount = 0;
@@ -47,7 +49,25 @@ def check_modules(journal, modules, lsb_modules, syspath_org):
         tcount = tcount + 1
         if journal:
             journal.purpose_start('Check ' + key)
-        
+
+        if key in lsb_modules:
+            appeared = lsb_modules[key][0]
+            withdrawn = lsb_modules[key][1]
+            #print appeared, withdrawn
+            if withdrawn != "NULL":
+                if float(withdrawn) < float(lsb_version):
+                    tresult = key + twithdrawn + withdrawn
+                    jresult = tetj.TETJ_FAIL              
+                    print tresult
+                    if journal:                
+                        journal.testcase_info(0,0,0,tresult)
+            if float(appeared) > float(lsb_version):
+                tresult = key + tappeared + appeared
+                jresult = tetj.TETJ_FAIL              
+                print tresult
+                if journal:                
+                    journal.testcase_info(0,0,0,tresult)
+
         if key not in lsb_modules:
             if key != '__main__' and key != '__builtin__':
                 # warn if we're getting modules from outside sys.path
@@ -87,12 +107,21 @@ def main(argv):
     journal = 0
     if '-j' in argv:
         journal = 1
-        testapp = argv[2]
-        jsuffix = os.path.basename(testapp)
-    elif '-v' in argv:
-        lsb_version = argv[3]
-    else:
-        testapp = argv[1]
+    if '-v' in argv:
+        vloc = string.index(argv, "-v")
+        lsb_version = argv[vloc + 1]
+        try:
+            float(lsb_version)
+        except:
+            print 'Invalid LSB_VERSION: ' + lsb_version
+            sys.exit(1)
+        if float(lsb_version) < 1.0 or float(lsb_version) > 20:
+            print 'Invalid LSB_VERSION: ' + lsb_version
+            sys.exit(1)
+
+    testapp = argv[len(argv)-1]
+    jsuffix = os.path.basename(testapp)
+    fullargs = string.join(argv, ' ')
 
     try:
         f = file(testapp, 'r')
@@ -104,7 +133,7 @@ def main(argv):
     # prep the journal header
     if journal:
         myname = os.path.basename(argv[0])
-        journal = tetj.Journal('journal.' + myname + '.' + jsuffix, testapp)
+        journal = tetj.Journal('journal.' + myname + '.' + jsuffix, fullargs)
         journal.add_config("VSX_NAME=lsb-tetjtest.py 0.1 (%s)" % journal.machine)
         journal.config_end()
 
@@ -113,14 +142,15 @@ def main(argv):
     exclude = []
     
     # get the 'blessed' LSB module list
-    lsb_modules = []
+    lsb_modules = {}
     modfile = open(sharepath + '/lsb-python-modules.list', 'r')
     line = modfile.readline()
     line = line.rstrip()
     while line:
         if line[0] != '#':
-            lsb_modules.append(line)
-            exclude.append(line)
+            module, appeared, withdrawn = string.split(line)
+            exclude.append(module)
+            lsb_modules[module] = appeared, withdrawn
         line = modfile.readline()
         line = line.rstrip()
     modfile.close()
@@ -135,13 +165,13 @@ def main(argv):
         print 'Cannot analyse ' + testapp + ' (is it a python app?)'
         sys.exit(1)
 
-    print testapp + ":"
+    print testapp + " tested against LSB " + lsb_version + ":"
     if journal:
         journal.testcase_start(testapp)
         file_info(journal, testapp)
 
     modules = mf.report()
-    check_modules(journal, modules, lsb_modules, syspath_org)
+    check_modules(journal, modules, lsb_modules, syspath_org, lsb_version)
     if journal:
         journal.testcase_end(testapp)
         journal.close()
