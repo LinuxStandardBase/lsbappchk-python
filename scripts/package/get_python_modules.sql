cc010e0309b129bc6179b1baff58792c3309792f
Stew Benedict <stewb@linux-foundation.org>
2011-01-06 18:32:49 -0500

    Make the default 4.1 now Gather deprecation state in sql - still needs handling in code Tweak the sql a bit, we want >= 3.2, not <= 3.2

--- scripts/package/get_python_modules.sql
+++ scripts/package/get_python_modules.sql
@@ -1,5 +1,5 @@
-select ILMname, ILMappearedin, ILMwithdrawnin from InterpretedLanguageModule 
+select ILMname, ILMappearedin, ILMwithdrawnin, ILMdeprecatedsince from InterpretedLanguageModule 
 where ILMlanguage in (select ILid from InterpretedLanguage 
 where ILname = 'Python')
-and ILMappearedin <>'' and ILMappearedin <= '3.2'
-and (ILMwithdrawnin is NULL or ILMwithdrawnin > '3.2')
+and ILMappearedin <>'' and ILMappearedin >= '3.2'
+and (ILMwithdrawnin is NULL or ILMwithdrawnin >= '3.2')
